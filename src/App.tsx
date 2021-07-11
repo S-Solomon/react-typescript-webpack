0f7a6d9e36f9fa3170ec23fe7684b1b7376df89b
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-07-11 17:17:42 +0100

    onclick buton

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ export const App = () => {
         <div className="app">
             <h1 className={ count > 0 ? 'positive': count < 0 ? 'negative' : undefined}>{count}</h1>
             <div className="button-wrapper">
-                <button onClick={() => setCount(count -1)}>-</button>
-                <button onClick={() => setCount(count + 1)}>+</button>
+                <button onClick={() => setCount(count => count -1)}>-</button>
+                <button onClick={() => setCount(count => count + 1)}>+</button>
             </div>
         </div>
     )
