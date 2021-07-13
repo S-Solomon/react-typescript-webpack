722e92e4d36e99e755b68fe17ca24d9e55377576
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-07-13 07:48:51 +0100

    useState types

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './App.scss'
 import { useState } from 'react'
 
-export const App = () => {
-    const [count, setCount] = useState(0)
+export const App: React.FC = () => {
+    const [count, setCount] = useState<number>(0)
     return (
         <div className="app">
             <h1
